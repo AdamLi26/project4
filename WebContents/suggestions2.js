3aae1737a787bc5db452aa628699ec5451d1f61e
baixiaobest <baixiaohuang1@gmail.com>
2016-02-26 13:09:58 -0800

    fix auto suggestion bug

--- WebContents/suggestions2.js
+++ WebContents/suggestions2.js
@@ -5,8 +5,12 @@
  * @scope public
  */
 
-function StateSuggestions(suggestions) {
-    this.states = suggestions;
+function StateSuggestions(httprequest) {
+    this.states = [];
+    this.httpRequest = httprequest;
+    this.sTextboxValue = null;
+    this.suggestionControl=null;
+    this.typeAhead=null;
 }
 
 /**
@@ -16,19 +20,40 @@ function StateSuggestions(suggestions) {
  */
 StateSuggestions.prototype.requestSuggestions = function (oAutoSuggestControl /*:AutoSuggestControl*/,
                                                           bTypeAhead /*:boolean*/) {
-    var aSuggestions = [];
-    var sTextboxValue = oAutoSuggestControl.textbox.value;
-    
-    if (sTextboxValue.length > 0){
-    
-        //search for matching states
-        for (var i=0; i < this.states.length; i++) { 
-            if (this.states[i].indexOf(sTextboxValue) == 0) {
-                aSuggestions.push(this.states[i]);
-            } 
-        }
-    }
+    this.sTextboxValue = oAutoSuggestControl.textbox.value;
+    this.suggestionControl = oAutoSuggestControl;
+    this.typeAhead = bTypeAhead;
 
-    //provide suggestions to the control
-    oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+    var request = "suggest?q="+encodeURI(this.sTextboxValue);
+    this.httpRequest.open("GET", request);
+    this.httpRequest.onreadystatechange = this.callBack.bind(this);
+    this.httpRequest.send(null);
+    
+    
 };
+
+StateSuggestions.prototype.callBack = function(){
+    if (xmlHttp.readyState == 4) {
+        console.log(this.httpRequest.responseXML);
+        var s = this.httpRequest.responseXML.getElementsByTagName('CompleteSuggestion');
+        var aSuggestions = [];
+        this.state = [];
+        for (i = 0; i < s.length; i++) {
+            this.states.push(s[i].childNodes[0].getAttribute("data"));
+        }
+
+        if (this.sTextboxValue.length > 0){
+    
+            //search for matching states
+            for (var i=0; i < this.states.length; i++) { 
+                if (this.states[i].indexOf(this.sTextboxValue) == 0) {
+                    aSuggestions.push(this.states[i]);
+                } 
+            }
+        }
+
+        //provide suggestions to the control
+        this.suggestionControl.autosuggest(aSuggestions, this.typeAhead);
+    }
+}
+
